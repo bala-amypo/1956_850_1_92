e35810019717511a4db6460ab1c4932eb9a8cffe
bala-amypo <bala@amypo.in>
2025-12-26 16:18:14 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,24 +1,19 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-
-        Server server = new Server()
-                .url("https://9257.408procr.amypo.ai/")
-                .description("Remote HTTPS Server");
-
         return new OpenAPI()
-                .servers(List.of(server));
+                .info(new Info()
+                        .title("Crime Hotspot Pattern Detector API")
+                        .version("1.0.0")
+                        .description("API documentation for Crime Hotspot Pattern Detector"));
     }
 }
-j
